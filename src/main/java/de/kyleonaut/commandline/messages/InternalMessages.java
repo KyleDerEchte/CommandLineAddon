9e14012f38bd3819d22d851c583f11b5fa93378e
Kyle <kyle.kreuter2202@gmail.com>
2021-02-24 10:09:03 +0100

    fixed InternalMessages

--- src/main/java/de/kyleonaut/commandline/messages/InternalMessages.java
+++ src/main/java/de/kyleonaut/commandline/messages/InternalMessages.java
@@ -1,8 +1,8 @@
 package de.kyleonaut.commandline.messages;
 
 public class InternalMessages {
-    private static String prefix = "§7[§6Command§3Line§7]";
-    public static String addonWasEnabled = ""+prefix+" §aDas Addon wurde aktiviert!";
-    public static String addonIsNotOnline = ""+prefix+" §cDas Addon ist nicht aktiviert!";
-    public static String addonWasDisabled = ""+prefix+" §aDas Addon wurde deaktiviert!";
+    private static String prefix = "§7[§6Command§3Line§7] ";
+    public static String addonWasEnabled = ""+prefix+"§aDas Addon wurde aktiviert!";
+    public static String addonIsNotOnline = ""+prefix+"§cDas Addon ist nicht aktiviert!";
+    public static String addonWasDisabled = ""+prefix+"§aDas Addon wurde deaktiviert!";
 }
